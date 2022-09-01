bb4b96c84f4d237a20c73b46ab5060383f10fb0e
yusragcelikel <yusragcelikel@gmail.com>
2022-09-01 12:31:00 +0300

    First Project

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,131 @@
+# string concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to ______"
+
+# youtuber= "Yusra Gokce Celikel" # some string variable
+
+# a few ways to do this
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+
+
+print("\n ____Mad libs game 1____ \n")
+
+adj_game1 = input("Adjective: ")
+verb1_game1 = input("First verb: ")
+verb2_game1 = input("Second verb: ")
+famous_person_game1 = input("Famous person: ")
+
+madlib_game1 = f"Computer programing is so {adj_game1}! It makes me so excited all the time because \n" \
+         f"I love to {verb1_game1}. Stay hydrated and {verb2_game1} like you are {famous_person_game1}!"
+print(madlib_game1)
+
+
+
+
+print("\n ____Mad libs game 2____ \n")
+
+holiday1 = input("Name a holiday: ")                                # 1
+noun1 = input("Name a noun or a thing : ")                          # 2
+verb1 = input("Write a past-tense verb: ")                          # 3
+country_name = input("Name a country: ")                            # 4
+number = input("Write a number: ")                                  # 5
+noun2 = input("Name a noun or a thing : ")                          # 6
+body_part1 = input("Name a body part : ")                           # 7
+exclamation1 = input("Name an exclamation word: ")                  # 8
+favorite_teacher = input("Write your favorite teacher: ")           # 9
+verb2 = input("Write a verb: ")                                     # 10
+noun3 = input("Name a noun: ")                                      # 11
+food_item = input("Name a food item: ")                             # 12
+store_name = input("Write a store name: ")                          # 13
+mode_of_transportation = input("Name a mode of transportation: ")   # 14
+noun4 = input("Name a noun or a thing: ")                           # 15
+adjective1 = input("Write an adjective: ")                          # 16
+toy_name = input("Enter a toy: ")                                   # 17
+storm_type = input("Write a storm type: ")                          # 18
+verb3 = input("Write a verb ending in -ing: ")                      # 19
+animal_name1 = input("Name a plural animal name: ")                 # 20
+animal_name2 = input("Name another plural animal name: ")           # 21
+room_name = input("Name a room in a building: ")                    # 22
+noun5 = input("Name a noun or a thing: ")                           # 23
+exclamation2 = input("Name an exclamation word: ")                  # 24
+electrical_appliance = input("Name an electrical appliance: ")      # 25
+animal_name3 = input("Name another plural animal name: ")           # 26
+verb4 = input("Write a verb: ")                                     # 27
+body_part2 = input("Name a body part : ")                           # 28
+adjective2 = input("Write an adjective: ")                          # 29
+famous_musician = input("Name a famous musician: ")                 # 30
+adjective3 = input("Write an adjective: ")                          # 31
+song_name = input("Name a song: ")                                  # 32
+body_part3 = input("Name a body part: ")                            # 33
+holiday2 = input("Name a holiday: ")                                # 34
+
+
+madlib_game2 = f"Mrs. Claus was baking {holiday1} cookies today when all of a sudden the {noun1} {verb1}. \n" \
+         f"Santa was in {country_name} working on {number} {noun2}. \n" \
+         f"Therefore Mrs. Claus didn’t know what to do. \n" \
+         f"\n" \
+         f"Not to mention, she couldn’t find a {body_part1} to fix the {noun1}. \n" \
+         f"{exclamation1}! At any rate, all the cookies burned! \"And {favorite_teacher} \n" \
+         f"won’t be able to {verb2} without the cookies!\" she cried. \n" \
+         f"\n" \
+         f"So Tinsel the Elf, who was busy decking the halls with {noun3}, \n" \
+         f"offered to get more {food_item} from {store_name} for Mrs. Claus to make \n" \
+         f"another batch of cookies. But on the way home, the {mode_of_transportation} \n" \
+         f"got stuck in {noun4}. What a conundrum! Thankfully Tinsel had a(n) \n" \
+         f"{adjective1} {toy_name} in her pocket! She used it to reach Rudolph, \n" \
+         f"who came to her rescue, and flew her home with the {food_item} as a result.\n" \
+         f"\n" \
+         f"But Wait!\n" \
+         f"No sooner had Rudolph and Tinsel returned home than a {storm_type} hit. \n" \
+         f"All of a sudden, it was {verb3} {animal_name1} and \n" \
+         f"{animal_name2}. Straightaway, everyone dashed inside to the {room_name} \n" \
+         f"to warm by the fire, where Mrs. Claus sat in her favorite {noun5}, \n" \
+         f"mixing up another batch of cookie dough. Wouldn’t you know that just as she was ready \n" \
+         f"to bake those cookies, the power went out?!  {exclamation2}! Now how would Mrs. Claus bake \n" \
+         f"if the {electrical_appliance} wouldn’t work?\n" \
+         f"\n" \
+         f"But just in the nick of time, who to my wondering eyes should appear but Mr. Claus and \n" \
+         f"eight tiny {animal_name3}! What’s more, with a {verb4} of his {body_part2} and his {adjective2} \n" \
+         f"Christmas magic, the power came back on. As a result, Mrs. Claus’s {electrical_appliance} " \
+         f"was working once again!\n" \
+         f"\n" \
+         f"Afterward, {famous_musician}, who had arrived in town just before the storm \n" \
+         f"to see if he/she was on the {adjective3} list, celebrated by singing {song_name} while Santa \n" \
+         f"loaded his sleigh to deliver the {holiday1} cookies to {favorite_teacher}.\n" \
+         f"\n" \
+         f"Well, bless my {body_part3}! It was a {holiday2} miracle to be sure!\n"
+
+print(madlib_game2)
+
+
+
+print("\n ____Mad libs game 3____ \n")
+
+adjective1_game3 = input("Write an adjective: ")
+adjective2_game3 = input("Write another adjective: ")
+type_of_bird = input("Name a type of bird: ")
+room_name_game3 = input("Name a room in a house: ")
+verb1_game3 = input("Write a past-tense verb: ")
+verb2_game3 = input("Write a verb: ")
+relative_name = input("Enter a relative's name: ")
+noun1_game3 = input("Name a noun: ")
+liquid_name = input("Name a liquid: ")
+verb3_game3 = input("Write a verb ending in -ing: ")
+body_part1_game3 = input("Name plural body part: ")
+noun2_game3 = input("Name plural noun: ")
+verb4_game3 = input("Write a verb ending in -ing: ")
+noun3_game3 = input("Name a noun: ")
+
+
+madlib_game3 = f"It was a(n) {adjective1_game3}, cold November day. I woke up to the {adjective2_game3} \n" \
+               f"smell of {type_of_bird} roasting in the {room_name_game3} downstairs. \n" \
+               f"I {verb1_game3} down the stairs to see if I could help {verb2_game3} the dinner. \n" \
+               f"My mom said, \"See if {relative_name} needs a fresh {noun1_game3}.\" So I carried \n" \
+               f"a tray of glasses full of {liquid_name} into the {verb3_game3} room. When I got \n" \
+               f"there, I couldn't believe my {body_part1_game3}! There were {noun2_game3} {verb4_game3} \n" \
+               f"on the {noun3_game3}!"
+
+print(madlib_game3)
