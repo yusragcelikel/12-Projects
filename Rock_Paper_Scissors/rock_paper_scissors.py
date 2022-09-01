9099f7bf5be46fc358bfa67f971bd9208132a867
yusragcelikel <yusragcelikel@gmail.com>
2022-09-01 14:07:14 +0300

    Fourth Project

---
+++ Rock_Paper_Scissors/rock_paper_scissors.py
@@ -0,0 +1,21 @@
+import random
+
+def play():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors \n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return "It's a  tie."
+
+    # ______r > s, s > p, p > r______
+    if is_win(user, computer):
+        return "You won!"
+  # if is_win(computer, user):   we don't have to say that.
+    return "You lost!"
+
+def is_win(player, opponent):
+    # return true if player wins
+    # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+print(play())
